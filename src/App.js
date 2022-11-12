43918bbd84fb134f891bf937cc9a88eacb896c0c
YavuzMollahamzaoglu <yavuz.mersin@hotmail.com>
2022-11-12 02:03:32 +0100

    Add rick and morty router app

--- src/App.js
+++ src/App.js
@@ -5,16 +5,11 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import { CardActionArea, useThemeProps } from "@mui/material";
 import { display } from "@mui/system";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  Router,
-  Link,
-  NavLink,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link, NavLink } from "react-router-dom";
+import ResponsiveAppBar from "./Components/Nav";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   // charaters
@@ -34,61 +29,66 @@ function App() {
   });
 
   return (
-    <div className="App" style={{ justifyContent: "center", display: "flex" }}>
-      <Navigation></Navigation>
-      <Typography>This is rick and morty app for my fellas</Typography>
-
-      <Grid container spacing={1}>
-        {characters.map((character) => {
-          return (
-            <Grid item xs={12} lg={4}>
-              <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="200"
-                    image={character.image}
-                    alt="green iguana"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {character.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {character.species}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-          );
-        })}
-      </Grid>
-    </div>
+    <Router>
+      <div>
+        <div className="Nav">
+          <ResponsiveAppBar></ResponsiveAppBar>
+        </div>
+        <div
+          className="App"
+          style={{ justifyContent: "center", display: "flex", marginLeft: 0 }}
+        >
+          <div style={{ marginLeft: 200, marginTop: 50 }}>
+            <Route
+              path="/"
+              exact
+              render={() => (
+                <Grid container spacing={1}>
+                  {characters.map((character) => {
+                    return (
+                      <Link to={`/character/${character.id}`}>
+                        <Grid item xs={12} lg={4}>
+                          <Card sx={{ maxWidth: 345 }}>
+                            <CardActionArea>
+                              <CardMedia
+                                component="img"
+                                height="200"
+                                image={character.image}
+                                alt="green iguana"
+                              />
+                              <CardContent>
+                                <Typography
+                                  gutterBottom
+                                  variant="h5"
+                                  component="div"
+                                >
+                                  {character.name}
+                                </Typography>
+                                <Typography
+                                  variant="body2"
+                                  color="text.secondary"
+                                >
+                                  {character.species}
+                                </Typography>
+                              </CardContent>
+                            </CardActionArea>
+                          </Card>
+                        </Grid>
+                      </Link>
+                    );
+                  })}
+                </Grid>
+              )}
+            />
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
-function Navigation() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/characters">Characters</NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function Characters() {
-  return <h2>Characters</h2>;
-}
+const character = (props) => {
+  return <div>Karakter Bilgileri</div>;
+};
 
 export default App;
